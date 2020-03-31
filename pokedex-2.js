0b23094ae8cc9a1b927b8b9fec9fdce54ed58e35
Mario Martinez <mario.martinez@peoplewalking.com>
2020-03-31 14:03:37 -0600

    Add detail when click on types

--- pokedex-2.js
+++ pokedex-2.js
@@ -1,9 +1,12 @@
 const pokemonsNode = document.querySelector("#pokemons");
 const pokemonNode = document.querySelector("#pokemon");
+const detailNode = document.querySelector("#detail");
 
 pokemonsNode.addEventListener("click", clickHandler);
 
-function getPokemons() {
+pokemonNode.addEventListener("click", pokemonHandler);
+
+function fetchPokemons() {
   pokemonsNode.textContent = "Is loading...";
 
   fetch("https://pokeapi.co/api/v2/pokemon/")
@@ -14,6 +17,19 @@ function getPokemons() {
       renderPokemons(data.results);
     })
     .catch(error => alert(error.message));
+}
+
+function fetchPokemon(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(pokemon => renderPokemon(pokemon));
+}
+
+function fetchPokemonDetail(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(detail => renderDetail(detail))
+    .catch(error => console.error(error.message));
 }
 
 function renderPokemons(pokemons) {
@@ -30,12 +46,6 @@ function renderPokemons(pokemons) {
   }
 }
 
-function fetchPokemon(url) {
-  fetch(url)
-    .then(response => response.json())
-    .then(pokemon => renderPokemon(pokemon));
-}
-
 function renderPokemon(pokemon) {
   const name = pokemon.name;
   const sprites = pokemon.sprites;
@@ -47,6 +57,8 @@ function renderPokemon(pokemon) {
 
   // clean pokemon node content
   pokemonNode.innerHTML = "";
+  // clean pokemon detail node content
+  detailNode.innerHTML = "";
 
   // append content
   pokemonNode.append(h1);
@@ -85,6 +97,100 @@ function renderPokemon(pokemon) {
   // append moves
   pokemonNode.append(movesTitle);
   pokemonNode.append(moves);
+}
+
+function renderDetail(detail) {
+  const {
+    double_damage_from: doubleDamageFrom,
+    double_damage_to: doubleDamageTo,
+    half_damage_from: halfDamageFrom,
+    half_damage_to: halfDamageTo,
+    no_damage_from: noDamageFrom,
+    no_damage_to: noDamageTo
+  } = detail.damage_relations;
+
+  // Clean node content
+  detailNode.innerHTML = "";
+
+  if (doubleDamageFrom.length) {
+    // double damage from
+    const doubleDamageFromTitle = getFeaturesTitle(
+      "Recibe doble daño de tipos"
+    );
+    const doubleDamageFromNode = getDamagesTypes(doubleDamageFrom);
+
+    // append double damage from
+    detailNode.append(doubleDamageFromTitle);
+    detailNode.append(doubleDamageFromNode);
+  }
+
+  if (doubleDamageTo.length) {
+    // double damage to
+    const doubleDamageToTitle = getFeaturesTitle("Hace doble daño a tipos");
+    const doubleDamageToNode = getDamagesTypes(doubleDamageTo);
+
+    // append double damage to
+    detailNode.append(doubleDamageToTitle);
+    detailNode.append(doubleDamageToNode);
+  }
+
+  if (halfDamageFrom.length) {
+    // half damage from
+    const halfDamageFromTitle = getFeaturesTitle(
+      "Recibe mitad de daño de tipos"
+    );
+    const halfDamageFromNode = getDamagesTypes(halfDamageFrom);
+
+    // append half damage from
+    detailNode.append(halfDamageFromTitle);
+    detailNode.append(halfDamageFromNode);
+  }
+
+  if (halfDamageTo.length) {
+    // half damage to
+    const halfDamageToTitle = getFeaturesTitle("Hace mitad de daño a tipos");
+    const halfDamageToNode = getDamagesTypes(halfDamageTo);
+
+    // append half damage to
+    detailNode.append(halfDamageToTitle);
+    detailNode.append(halfDamageToNode);
+  }
+
+  if (noDamageFrom.length) {
+    // no demage from
+    const noDamageFromTitle = getFeaturesTitle("No recibe  daño de tipos");
+    const noDamageFromNode = getDamagesTypes(noDamageFrom);
+
+    // append half damage to
+    detailNode.append(noDamageFromTitle);
+    detailNode.append(noDamageFromNode);
+  }
+
+  if (noDamageTo.length) {
+    // no demage to
+    const noDamageToTitle = getFeaturesTitle("No Hace  daño a tipos");
+    const noDamageToNode = getDamagesTypes(noDamageTo);
+
+    // append half damage to
+    detailNode.append(noDamageToTitle);
+    detailNode.append(noDamageToNode);
+  }
+}
+
+function getDamagesTypes(items) {
+  const ul = document.createElement("ul");
+
+  items
+    .map(item => item.name)
+    .forEach(name => {
+      const li = document.createElement("li");
+
+      li.append(name);
+
+      ul.append(li);
+    });
+
+  return ul;
 }
 
 function getTypes(typeList) {
@@ -140,5 +246,17 @@ function clickHandler(event) {
   fetchPokemon(target.getAttribute("href"));
 }
 
+function pokemonHandler(event) {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.nodeName !== "A") {
+    return false;
+  }
+
+  fetchPokemonDetail(target.href);
+}
+
 // initial function
-getPokemons();
+fetchPokemons();
